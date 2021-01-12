a605eacd08ab8fe5259cd8448d29c08411f8006c
Rurik Pinheiro <rurik@fullstacklabs.co>
2021-01-12 15:59:50 -0300

    feat: enable typescript support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+import { AppProps } from "next/app";
+
 import "../styles/globals.css";
 import "materialize-css/dist/css/materialize.min.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
